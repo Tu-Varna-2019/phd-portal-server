85fed5c9ca40d9de2af0ad6f1116a919d22de083
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-11-10 22:16:26 +0200

    feat: separated controller to teacher

--- src/main/java/com/tuvarna/phd/controller/TeacherController.java
+++ src/main/java/com/tuvarna/phd/controller/TeacherController.java
@@ -4,12 +4,10 @@ import com.tuvarna.phd.entity.Teacher;
 import com.tuvarna.phd.service.TeacherService;
 import com.tuvarna.phd.service.dto.TeacherDTO;
 import jakarta.annotation.security.PermitAll;
-import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -20,40 +18,25 @@ import jakarta.ws.rs.core.MediaType;
 // import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 @RequestScoped
-@Path("/contacts")
+@Path("/teacher")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 // @SecurityScheme(securitySchemeName = "Basic Auth", type = SecuritySchemeType.HTTP, scheme =
 // "basic")
-public class Contacts {
+public class TeacherController {
 
   private final TeacherService teacherService;
 
   @Inject
-  public Contacts(TeacherService teacherService) {
+  public TeacherController(TeacherService teacherService) {
     this.teacherService = teacherService;
-  }
-
-  @GET
-  @RolesAllowed({"USER", "ADMIN"})
-  // @Operation(summary = "Gets users", description = "Lists all available users")
-  // @APIResponses(value = @APIResponse(responseCode = "200", description = "Success",
-  //                 content = @Content(mediaType = "application/json", schema =
-  // @Schema(implementation = User.class))))
-  public String getContacts() {
-    return "Teachers";
   }
 
   @POST
   @PermitAll
   @Transactional
+  @Path("/create")
   public Teacher createTeacher(TeacherDTO teacherDTO) {
     return this.teacherService.save(teacherDTO.toTeacher());
   }
-
-  // @POST
-  // @PermitAll
-  //   public User createUser(@Valid UserDto userDto) {
-  //       return userService.saveUser(userDto.toUser());
-  //   }
 }
