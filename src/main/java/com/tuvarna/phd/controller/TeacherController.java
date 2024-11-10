b72aca6da0773ca418a53ae07be25ea9a8bfb172
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-11-10 22:49:24 +0200

    feat: added openapi spec

--- src/main/java/com/tuvarna/phd/controller/TeacherController.java
+++ src/main/java/com/tuvarna/phd/controller/TeacherController.java
@@ -4,6 +4,7 @@ import com.tuvarna.phd.entity.Teacher;
 import com.tuvarna.phd.service.TeacherService;
 import com.tuvarna.phd.service.dto.TeacherDTO;
 import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -12,17 +13,19 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-
-// import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
-// import org.eclipse.microprofile.openapi.annotations.Operation;
-// import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 
 @RequestScoped
 @Path("/teacher")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-// @SecurityScheme(securitySchemeName = "Basic Auth", type = SecuritySchemeType.HTTP, scheme =
-// "basic")
+@SecurityScheme(securitySchemeName = "Basic Auth", type = SecuritySchemeType.HTTP, scheme = "basic")
 public class TeacherController {
 
   private final TeacherService teacherService;
@@ -35,6 +38,17 @@ public class TeacherController {
   @POST
   @PermitAll
   @Transactional
+  @RolesAllowed({"USER", "ADMIN"})
+  @Operation(summary = "Create teacher", description = "Creates a teacher in the system")
+  @APIResponses(
+      value =
+          @APIResponse(
+              responseCode = "200",
+              description = "Success",
+              content =
+                  @Content(
+                      mediaType = "application/json",
+                      schema = @Schema(implementation = TeacherDTO.class))))
   @Path("/create")
   public Teacher createTeacher(TeacherDTO teacherDTO) {
     return this.teacherService.save(teacherDTO.toTeacher());
