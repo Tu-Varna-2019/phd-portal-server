b5875f4a5380a9ff86f4bcda827271d38caedda8
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-05-04 12:53:10 +0200

    feat: added ToString
    M  src/main/java/com/tuvarna/phd/entity/Curriculum.java
    M  src/main/java/com/tuvarna/phd/entity/Subject.java
    M  src/main/java/com/tuvarna/phd/mapper/CurriculumMapper.java
    M  src/main/java/com/tuvarna/phd/service/CandidateServiceImpl.java

--- src/main/java/com/tuvarna/phd/mapper/CurriculumMapper.java
+++ src/main/java/com/tuvarna/phd/mapper/CurriculumMapper.java
@@ -1,8 +1,13 @@
 package com.tuvarna.phd.mapper;
 
+import com.sun.istack.NotNull;
 import com.tuvarna.phd.dto.CurriculumCreateDTO;
 import com.tuvarna.phd.dto.CurriculumDTO;
 import com.tuvarna.phd.entity.Curriculum;
+import com.tuvarna.phd.entity.Subject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -11,7 +16,6 @@ import org.mapstruct.factory.Mappers;
 public interface CurriculumMapper {
   CurriculumMapper mapperInstance = Mappers.getMapper(CurriculumMapper.class);
 
-  @Mapping(target = "subjects", ignore = true)
   @Mapping(target = "yearPeriod", source = "curriculum.mode.yearPeriod")
   @Mapping(target = "mode", source = "curriculum.mode.mode")
   @Mapping(target = "faculty", source = "curriculum.faculty.name")
@@ -28,4 +32,12 @@ public interface CurriculumMapper {
   @Mapping(target = "mode", ignore = true)
   @Mapping(target = "faculty.name", source = "faculty")
   Curriculum toEntity(CurriculumCreateDTO curriculumDTO);
+
+  @NotNull
+  default List<String> map(Set<Subject> subjects) {
+    List<String> result = new ArrayList<>();
+    subjects.forEach((subject) -> result.add(subject.getName()));
+
+    return result;
+  }
 }
